5612a41bbf9062a0c5e19e050c7c5ea94f3309a9
MarcusNCo <levert.marc.a@gmail.com>
2023-03-20 09:15:41 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function Header(){
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
