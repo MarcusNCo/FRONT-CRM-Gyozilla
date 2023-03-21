27f5eb7f5d443a1b7ac3710a2ee5f2295e59638e
LegrandRemy <legrandremy19@outlook.fr>
2023-03-21 15:23:28 +0100

    customButton + customCard + debut Theming

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import Header from './components/Header'
 import './App.css'
-import logo from './images/Sans_titre-1_105_copie.png';
-import Products from './pages/products/Products';
-import './App.css';
-import Log from './pages/log/Log';
+import logo from './images/Sans_titre-1_105_copie.png'
+import Products from './pages/products/Products'
+import Log from './pages/log/Log'
+import CustomButton from './components/button/CustomButton'
+import { useState } from 'react'
+import CustomCard from './components/Card/CustomCard'
+import { Alert, Badge, ThemeProvider, useTheme } from '@mui/material'
+import defaultTheme from './utils/theming/theme'
+import MailIcon from '@mui/icons-material/Mail'
 
 function App() {
+  const [test, setTest] = useState('state initial')
+  const [test2, setTest2] = useState('state initial du bouton card')
+
+  const handleClick = () => {
+    setTest('changement de state initial')
+  }
+  const handleCardButtonClick = () => {
+    setTest2('changement de state initial du bouton card')
+  }
+  console.log(test, test2)
+  const theme = useTheme()
   return (
-    <div className="App">
-      <Header />
-      <div>WOUUUAAAAAAAAAhhhhhhh</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Log />
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Badge badgeContent={4}>
+        <MailIcon color="action" />
+      </Badge>
+      <Alert>waouaauauauauauauauuaua</Alert>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {/* <Log /> */}
+        <Products />
+        <CustomButton
+          variant="contained"
+          text="coucou"
+          color={'secondary'}
+          onClick={handleClick}
+        />
+        <CustomCard
+          sizeTitle="h5"
+          width="50%"
+          height={'100%'}
+          title="Titre de la card"
+          description="lorem ipsum uudsbifuq fuhsufqsidf uifqGQSD GIUNUI UFIU  U UFUDSGWGFBhfg iugnui ugs"
+          buttonCardText="Details de la Card"
+          onButtonCardClick={handleCardButtonClick}
+          backgroundColor="red"
+          colorParagraph={'blue'}
+          colorTitle={'toto'}
+        ></CustomCard>
+      </div>
+    </ThemeProvider>
   )
 }
 
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,51 +1,69 @@
 import React, { useEffect, useState } from 'react'
 import { getAllProducts } from '../../utils/getAllProducts'
-import CircularProgress from '@mui/material/CircularProgress';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import './Products.css'
 
-
 const Products = () => {
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null)
 
-    useEffect(() => {
-        getAllProducts()
-            .then((res) => {
-                setProducts(res.data);
-                setError(null);
-            })
-            .catch((error) => {
-                setProducts([]);
-                setError(error);
-            });
-        }, []);
-    return (
-        <div className='products'>
-            {
-                products.length ===0 ? (
-                    <CircularProgress/>
-                ) : products.map((item)=>{
-                    return (
-                        <Card sx={{ maxWidth: 345, background: "grey", border: '1px solid black', margin: '2px' }}>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {item.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {item.description}
-                                </Typography>
-                            </CardContent>
-                            <Button sx={{background: 'black', color: 'white', border: '1px solid white'}} variant="outlined">Commander</Button>
-                        </Card>
-                    )
-                })
-            }
-        </div>
-    )
+  useEffect(() => {
+    getAllProducts()
+      .then((res) => {
+        console.log(res)
+        setProducts(res.data)
+        setError(null)
+      })
+      .catch((error) => {
+        setProducts([])
+        setError(error)
+      })
+  }, [])
+  //console.log(products)
+  return (
+    <div className="products">
+      {products.length === 0 ? (
+        <CircularProgress />
+      ) : (
+        products.map((item) => {
+          return (
+            <Card
+              key={item.id}
+              sx={{
+                maxWidth: 345,
+                background: 'grey',
+                border: '1px solid black',
+                margin: '2px',
+              }}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <Button
+                sx={{
+                  background: 'black',
+                  color: 'white',
+                  border: '1px solid white',
+                }}
+                variant="outlined"
+              >
+                Commander
+              </Button>
+            </Card>
+          )
+        })
+      )}
+    </div>
+  )
 }
 
 export default Products
