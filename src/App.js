70ede13dd4f55066351d4505d7125063f0717e03
Cyril-Crinquette <twousy734@gmail.com>
2023-03-23 11:24:04 +0100

    maj file app

--- src/App.js
+++ src/App.js
@@ -55,22 +55,22 @@ function App() {
   return (
 <<<<<<< HEAD
 <<<<<<< HEAD
-    <div className="App">
+      <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
     </div>
 =======
 =======
