a87283f8ccac77372e54f0c55e4769cfbda00ad3
Cyril-Crinquette <twousy734@gmail.com>
2023-04-05 09:49:41 +0200

    maj avec correction des chemins

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,78 +1,98 @@
-import React, { useEffect, useState } from "react";
-import { getAllProducts } from "../../utils/api-call/getAllProducts";
-import CircularProgress from "@mui/material/CircularProgress";
+import React, { useEffect, useState } from 'react'
+import { getAllProducts } from '../../utils/api-call/getAllProducts'
+import CircularProgress from '@mui/material/CircularProgress'
 // import Card from '@mui/material/Card'
 // import CardContent from '@mui/material/CardContent'
 // import Button from '@mui/material/Button'
 // import Typography from '@mui/material/Typography'
-import "./Products.css";
-import CustomCard from "../../components/card/CustomCard";
+import './Products.css'
+import CustomCard from '../../components/Card/CustomCard'
 // import ListItemProducts from "../../components/customlistitem/CustomListItemProducts";
-import CustomListItemProducts from "../../components/customlistitem/CustomListItemProducts";
-import CustomButton from "../../components/button/CustomButton";
+import CustomListItemProducts from '../../components/CustomListItem/CustomListItemProducts'
+import CustomButton from '../../components/button/CustomButton'
 // import { Fab, IconButton } from "@mui/material";
-import { Fab } from "@mui/material";
-import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import { Fab } from '@mui/material'
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn'
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [selectedListItem, setSelectedListItem] = useState(0);
-  const [selected, setSelected] = useState(0);
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null)
+  const [selectedListItem, setSelectedListItem] = useState(0)
+  const [selected, setSelected] = useState(0)
   const categories = [
-    { id: 3, name: "Entrées", description: "Découvrez nos entrées", image: "entrees.jpg" },
-    { id: 4, name: "Plats", description: "Découvrez nos plats", image: "plats.jpg" },
-    { id: 5, name: "Desserts", description: "Découvrez nos desserts", image: "desserts.jpg" },
-    { id: 6, name: "Boissons", description: "Découvrez nos boissons", image: "boissons.jpg" },
-  ];
+    {
+      id: 3,
+      name: 'Entrées',
+      description: 'Découvrez nos entrées',
+      image: 'entrees.jpg',
+    },
+    {
+      id: 4,
+      name: 'Plats',
+      description: 'Découvrez nos plats',
+      image: 'plats.jpg',
+    },
+    {
+      id: 5,
+      name: 'Desserts',
+      description: 'Découvrez nos desserts',
+      image: 'desserts.jpg',
+    },
+    {
+      id: 6,
+      name: 'Boissons',
+      description: 'Découvrez nos boissons',
+      image: 'boissons.jpg',
+    },
+  ]
 
   useEffect(() => {
     getAllProducts()
       .then((res) => {
-        console.log(res);
-        setProducts(res.data);
-        setError(null);
+        console.log(res)
+        setProducts(res.data)
+        setError(null)
       })
       .catch((error) => {
-        setProducts([]);
-        setError(error);
-      });
-  }, []);
+        setProducts([])
+        setError(error)
+      })
+  }, [])
 
   const handleListItemClick = (event, index) => {
-    setSelectedListItem(index);
-  };
+    setSelectedListItem(index)
+  }
   //Dans BDD => product.productCategory.id 1:Entrees 2:Plats 3:Desserts 4:Boissons 5:Nouveautes
   // ListItem 1:Nouveautes 2:Les Menus 3:Entrees 4:Plats 5:Desserts 6:Boissons
   const filteredProducts = products.filter((product) => {
     if (selectedListItem === null || selectedListItem === 0) {
-      return true;
+      return true
     } else if (selectedListItem === 1) {
-      const today = new Date();
-      const yesterday = new Date(today);
-      yesterday.setDate(yesterday.getDate() - 1);
+      const today = new Date()
+      const yesterday = new Date(today)
+      yesterday.setDate(yesterday.getDate() - 1)
 
       return (
         new Date(product.createdAt) >= yesterday &&
         new Date(product.createdAt) <= today
-      );
+      )
     } else if (selectedListItem === 2) {
-      return true;
+      return true
     } else if (selectedListItem === 3) {
-      return product.productCategory.id === 1;
+      return product.productCategory.id === 1
     } else if (selectedListItem === 4) {
-      return product.productCategory.id === 2;
+      return product.productCategory.id === 2
     } else if (selectedListItem === 5) {
-      return product.productCategory.id === 3;
+      return product.productCategory.id === 3
     } else if (selectedListItem === 6) {
-      return product.productCategory.id === 4;
+      return product.productCategory.id === 4
     } else {
-      return null;
+      return null
     }
-  });
+  })
   return (
     <>
-      <div className="container" style={{ display: "flex" }}>
+      <div className="container" style={{ display: 'flex' }}>
         <CustomListItemProducts
           selected={selected}
           onClick={handleListItemClick}
@@ -82,9 +102,9 @@ const Products = () => {
         <div
           className="products"
           style={{
-            flexWrap: "wrap",
-            width: "100vw",
-            justifyContent: "space-evenly",
+            flexWrap: 'wrap',
+            width: '100vw',
+            justifyContent: 'space-evenly',
           }}
         >
           {selectedListItem === null || selectedListItem === 0 ? (
@@ -96,16 +116,22 @@ const Products = () => {
                   description={category.description}
                   title={category.name}
                   buttonCardText="Voir les produits"
-                  variantButton={"contained"}
+                  variantButton={'contained'}
                   onButtonCardClick={() => setSelectedListItem(category.id)}
                   width="250px"
                   height="250px"
                   image={category.image}
                 ></CustomCard>
-              );
+              )
             })
           ) : filteredProducts.length === 0 ? (
-            <CircularProgress sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} />
+            <CircularProgress
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            />
           ) : (
             filteredProducts.map((item) => {
               return (
@@ -114,17 +140,17 @@ const Products = () => {
                   description={item.description}
                   image={item.image}
                   buttonCardText="Details"
-                  variantButton={"contained"}
+                  variantButton={'contained'}
                   width="250px"
                   height="250px"
                   title={item.name}
                 ></CustomCard>
-              );
+              )
             })
           )}
         </div>
       </div>
-      
+
       {/* bouton retour en version desktop */}
       <CustomButton
         text="Retour"
@@ -148,7 +174,7 @@ const Products = () => {
         <KeyboardReturnIcon />
       </Fab> */}
     </>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
