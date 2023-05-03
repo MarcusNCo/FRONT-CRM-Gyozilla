3b5ff4dba9ce2e403c59e292038cb4f80d94ae7f
zokkyen <stephane.swaenepoel@wanadoo.fr>
2023-05-03 21:15:32 +0200

    little modif of Order

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
-
+import React, { useState, useEffect } from "react";
+import { Box, Typography, Button, CardMedia } from "@mui/material";
 
 const Order = () => {
-
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    const cartData = localStorage.getItem('cart');
+    const cartData = localStorage.getItem("cart");
     if (cartData) {
       const cartParsed = JSON.parse(cartData);
       setCartItems(Object.values(cartParsed));
@@ -22,26 +20,63 @@ const Order = () => {
     return dbImage;
   };
 
+  const getTotal = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const handleProceed = () => {
+    // Ajoutez votre logique pour passer à l'étape suivante de la commande
+    console.log("Poursuivre la commande");
+  };
+
   return (
     <>
       <Box
         sx={{
-          minHeight: 'calc(100vh - 71px)',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
+          minHeight: "calc(100vh - 71px)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+          alignItems: "center",
         }}
       >
-        <Typography variant="h7bnw" color="initial">Récapitulatif de la commande</Typography>
+        <Typography variant="h7bnw" color="initial">
+          Récapitulatif de la commande
+        </Typography>
         {cartItems.map((item) => (
           <Box key={item.id}>
-            <Typography variant="h7bnw" color="initial">{item.name}</Typography>
-            <img src={getImage(item.image)} alt={item.name} style={{ height: '150px', width: '150px' }} />
-            <Typography variant="body1" color="initial">Prix: {item.price}€</Typography>
-            <Typography variant="body1" color="initial">Quantité: {item.quantity}</Typography>
+            <Typography variant="h6" color="initial">
+              {item.name}
+            </Typography>
+
+            <CardMedia
+              className="around"
+              component="img"
+
+              image={getImage(item.image)}
+              alt={item.name}
+              sx={{
+                height:"140px",
+                width:"140px"
+              }}
+            />
+            <Typography variant="body1" color="initial">
+              Prix: {item.price}€
+            </Typography>
+            <Typography variant="body1" color="initial">
+              Quantité: {item.quantity}
+            </Typography>
           </Box>
         ))}
+        <Typography variant="h6" color="initial">
+          Total: {getTotal()}€
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handleProceed}>
+          Poursuivre la commande
+        </Button>
       </Box>
     </>
   );
