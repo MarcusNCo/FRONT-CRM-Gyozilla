b896e7dc86a3857282312a853bbcd9be9bc40b5d
Cyril-Crinquette <twousy734@gmail.com>
2023-06-26 09:35:38 +0200

    maj 26/06

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,82 +1,82 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/log/Login";
-import Products from "./pages/products/Products";
-import { Routes, Route } from "react-router-dom";
-import Verify from "./pages/verify/Verify";
-import ProductDetails from "./pages/productdetails/ProductDetails";
-import CreateUser from "./pages/createuser/CreateUser";
-import Profile from "./pages/profile/Profile";
-import ForgotPassword from "./pages/forgot-password/ForgotPassword";
-import ResetPassword from "./pages/reset-password/ResetPassword";
-import NosEngagements from "./pages/nosengagements/NosEngagements";
-import Menu from "./pages/menu/Menu";
-import Order from "./pages/order/Order";
-import Find from "./pages/find/Find";
-import Contact from "./pages/contact/Contact";
+import Home from './pages/home/Home'
+import Login from './pages/log/Login'
+import Products from './pages/products/Products'
+import { Routes, Route } from 'react-router-dom'
+import Verify from './pages/verify/Verify'
+import ProductDetails from './pages/productDetails/ProductDetails'
+import CreateUser from './pages/createuser/CreateUser'
+import Profile from './pages/profile/Profile'
+import ForgotPassword from './pages/forgot-password/ForgotPassword'
+import ResetPassword from './pages/reset-password/ResetPassword'
+import NosEngagements from './pages/nosengagements/NosEngagements'
+import Menu from './pages/menu/Menu'
+import Order from './pages/order/Order'
+import Find from './pages/find/Find'
+import Contact from './pages/contact/Contact'
 
 function RoutesConfig() {
   const routes = [
     {
-      path: "/",
+      path: '/',
       element: <Home />,
     },
     {
-      path: "/login",
+      path: '/login',
       element: <Login />,
     },
     {
-      path: "/profile",
+      path: '/profile',
       element: <Profile />,
     },
     {
-      path: "/createuser",
+      path: '/createuser',
       element: <CreateUser />,
     },
     {
-      path: "/forgot-password",
+      path: '/forgot-password',
       element: <ForgotPassword />,
     },
     {
-      path: "/reset-password",
+      path: '/reset-password',
       element: <ResetPassword />,
     },
     {
-      path: "/products",
+      path: '/products',
       element: <Products />,
     },
     {
-      path: "/products/:id",
+      path: '/products/:id',
       element: <ProductDetails />,
     },
     {
-      path: "/menu",
+      path: '/menu',
       element: <Menu />,
     },
     {
-      path: "/nosengagements",
+      path: '/nosengagements',
       element: <NosEngagements />,
     },
     {
-      path: "/contact",
+      path: '/contact',
       element: <Contact />,
     },
     {
-      path: "/sign-in",
+      path: '/sign-in',
       element: <CreateUser />,
     },
     {
-      path: "/verify/:token",
+      path: '/verify/:token',
       element: <Verify />,
     },
     {
-      path: "/order",
+      path: '/order',
       element: <Order />,
     },
     {
-      path: "/find",
+      path: '/find',
       element: <Find />,
     },
-  ];
+  ]
 
   return (
     <Routes>
@@ -84,7 +84,7 @@ function RoutesConfig() {
         <Route key={index} path={route.path} element={route.element} />
       ))}
     </Routes>
-  );
+  )
 }
 
-export default RoutesConfig;
+export default RoutesConfig
